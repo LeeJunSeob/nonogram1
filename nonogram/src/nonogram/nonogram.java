9182d6d5a6ce3a4ebe875b992b74b9d6c4b7fbc2
LeeJunSeob <jsv1130@kaist.ac.kr>
2019-05-29 18:20:29 +0900

    a
    a

--- nonogram/src/nonogram/nonogram.java
+++ nonogram/src/nonogram/nonogram.java
@@ -5,31 +5,29 @@ import lejos.hardware.motor.Motor;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.Keys;
 import lejos.hardware.ev3.EV3;
-import lejos.robotics.SampleProvider;
 import lejos.hardware.lcd.TextLCD;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.port.SensorPort;
 import lejos.robotics.Color;
-import lejos.hardware.sensor.EV3IRSensor;
 import lejos.utility.Delay;
 import lejos.hardware.Sound;
 
 import java.lang.String;
 
 public class nonogram {
-    private static EV3ColorSensor color;
+    //private static EV3ColorSensor color;
     static TextLCD lcd;
-    static RegulatedMotor stampMotor = Motor.A;		  // Moter A   forward:  Up stamp frame
+    static RegulatedMotor stampMotor = Motor.A;		    // Moter A   forward:  Up stamp frame
                                                         //           backward: Down stamp frame
     static RegulatedMotor verticalMotor = Motor.B;      // Moter B   forward:  Take out sheet toward initial point
                                                         //           backward: Take in sheet ``
-    static RegulatedMotor horizontalMotor = Motor.C;	  // Moter C   backward:  move stamp frame toward vertical moter
+    static RegulatedMotor horizontalMotor = Motor.C;	// Moter C   backward:  move stamp frame toward vertical moter
                                                         //           forward: move stamp frame counterwise
-    static int[][] map = {{0,1,1,0,1},
-                          {1,1,0,0,0},
-                          {0,0,0,1,0},
-                          {1,0,1,0,1},
-                          {0,1,0,1,0}};
+    static int[][] map5 = {{1,1,1,1,1},
+                          {1,1,1,1,1},
+                          {1,1,1,1,1},
+                          {1,1,1,1,1},
+                          {1,1,1,1,1}};
 
     static int[][] map7 = {{1,1,1,1,1,1,1},
                            {1,1,1,1,1,1,1},
@@ -39,16 +37,16 @@ public class nonogram {
                            {1,1,1,1,1,1,1},
                            {1,1,1,1,1,1,1}};
 
-    static int[][] map10 = {{1,1,1,1,1,1,1,1,1,1},
+    static int[][] map10 = {{0,1,1,1,0,0,1,1,1,0},
+                            {1,1,0,1,1,1,1,0,1,1},
+                            {1,0,0,0,1,1,0,0,0,1},
+                            {1,0,0,0,0,0,0,0,0,1},
+                            {1,1,0,0,0,0,0,0,1,1},
+                            {0,1,1,0,0,0,0,1,1,0},
+                            {0,0,1,1,0,0,1,1,0,0},
+                            {0,0,0,1,1,1,1,0,0,0},
                             {1,1,1,1,1,1,1,1,1,1},
-                            {1,1,1,1,1,1,1,1,1,1},
-                            {1,1,1,1,1,1,1,1,1,1},
-                            {1,1,1,1,1,1,1,1,1,1},
-                            {1,1,1,1,1,1,1,1,1,1},
-                            {1,1,1,1,1,1,1,1,1,1},
-                            {1,1,1,1,1,1,1,1,1,1},
-                            {1,1,1,1,1,1,1,1,1,1},
-                            {1,1,1,1,1,1,1,1,1,1}};
+                            {0,1,1,1,1,1,1,1,1,0}};
 
     public static void main (String[] args)
     {
@@ -56,72 +54,72 @@ public class nonogram {
         Keys keys = ev3.getKeys();
 //        color = new EV3ColorSensor(SensorPort.S4);
         lcd = ev3.getTextLCD();
-        stampMotor.setSpeed(20);
-        stampMotor.rotate(50);
-        stampMotor.rotate(-8);
-        horizontalMotor.setSpeed(400);
-        makeNonogram (map10);
+        
+        int[][] usemap = map10;
+        
+        /*
+        for (int i = 0; i < usemap.length; i++)
+        	for (int j = 0; j < usemap[i].length; j++)
+        		usemap[i][j] = (i + j) % 2;
+        */
+        initialize ();
+        makeNonogram (usemap);
+        end ();
     }
+    
+	public static void initialize ()
+	{
+		stampMotor.setSpeed(25);
+		verticalMotor.setSpeed(100);
+		horizontalMotor.setSpeed(400);
 
-    public static void stamp()
+        //stampMotor.rotate(-10);
+        
+        verticalMotor.rotate(-200);
+	}
+	
+	public static void end ()
+	{
+		horizontalMotor.rotate (-1200);
+		horizontalMotor.rotate (600);
+	}
+	
+    public static void stamp() throws InterruptedException
     {
-        stampMotor.rotate (8);
-        stampMotor.rotate (-8);
+        stampMotor.rotate (15);
+        Thread.sleep(300);
+        stampMotor.rotate (-15);
     }
-
+    
+    /* func that move vertical until starting point */
     public static void makeNonogram(int[][] list)
     {
-        horizontalMotor.setSpeed(400);
-        verticalMotor.rotate(-150);
-
-        //func that move vertical until starting point
-
-        //stamping that list index is 1
         for(int i = 0; i < list.length; i++)
         {
             horizontalMotor.rotate(1200);//align left
-            horizontalMotor.rotate(-50);
-
-            for(int j = 0; j < list[i].length; j++)
+            horizontalMotor.rotate(-140);
+            
+            int prev = 0;
+            for(int j = list[i].length; j >= 0; j--)
             {
-                horizontalMotor.rotate(-100);//move horizontal
 
-                if(list[i][j]==1)
+                if(list[i][j] == 1)
+                {
+                	horizontalMotor.rotate(-66 * (j - prev));
                     stamp();
+                    prev = j;
+                }
             }
 
-            verticalMotor.rotate(-65);//move vertical
+            verticalMotor.rotate(-43);//move vertical
         }
     }
     
-//	  	public static void initialize ()
-//	    {
-//	      stampMoter.setSpeed(100); 	  		        	// * ARGUMENT MUST BE TESTED *
-//	      verticalMoter.setSpeed(50); 				        // * ARGUMENT MUST BE TESTED *
-//	      horizontalMoter.setSpeed(50); 			        // * ARGUMENT MUST BE TESTED *
-//	      
-//	      horizontalMoter.forward();
-//	      horizontalMoter.rotate (720); 				    // * ARGUMENT MUST BE TESTED *
-//	      horizontalMoter.rotate (-360); 			        // * ARGUMENT MUST BE TESTED *
-//	      verticalMoter.backward();
-//	      verticalMoter.move();
-//
-//	      while (color.getcolorID() != Color.BLACK);
-//	      
-//	      verticalMoter.stop();
-//	      
-//	      /* Following codes make stampMoter initialized.
-//	       * For avoid making dirty in blanks of sheet, Sheet must be taken out a few, 
-//	       * then check stampMoter and get back to original point. */
-//	      verticalMoter.rotate (180);
-//	      stampMoter.rotate (-180);
-//	      stampMoter.rotate (90);
-//	      verticalMoter.rotate (-180);
-//
-//	      Thread.sleep (1000);
-//	    }
-
- 
-
-
+//    public static int[][] stringtoList(String recv){
+//    	for(int i=0;i<10;i++){
+//    		for(int j=0;j<10;j++){
+//    			recv.charAt(10*i+j);
+//    		}
+//    	}
+//    }
 }
